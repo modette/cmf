d59085ffa4ea2dd79f361f2b6ca8199ff107ad5b
Marek Bartos <bartos.developer152@gmail.com>
2018-10-21 22:29:21 +0200

    Disable temporarily

--- packages/module-installer/src/Plugin.php
+++ packages/module-installer/src/Plugin.php
@@ -5,8 +5,6 @@ namespace Modette\ModuleInstaller;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Nette\Neon\Neon;
-use function class_exists;
 
 class Plugin implements PluginInterface
 {
@@ -14,9 +12,9 @@ class Plugin implements PluginInterface
 	public function __construct()
 	{
 		// Neon::class not exists on initial installation so internal is used
-		if (!class_exists(Neon::class)) {
-			require __DIR__ . '/../vendor/autoload.php';
-		}
+		//if (!class_exists(Neon::class)) {
+		//	require __DIR__ . '/../vendor/autoload.php';
+		//}
 	}
 
 	/**
