0cc444ab3a2f23fc777f1098fc746a9406a8e211
Marek Bartoš <bartos.developer152@gmail.com>
2019-10-03 20:10:50 +0200

    [Project] Move boot files under src folder

--- packages/project/src/Bootstrap.php
+++ packages/project/src/Bootstrap.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace Boot;
+namespace App;
 
 use Modette\Core\Boot\Configurator;
 use Modette\Core\Boot\Helper\EnvironmentHelper;
--- packages/project/src/ConfigLoader.php
+++ packages/project/src/ConfigLoader.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace Boot;
+namespace App;
 
 use Modette\ModuleInstaller\Loading\Loader;
 
