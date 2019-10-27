efec5e5e2c0d9a3bf490225937d94c4dba997a2f
Marek Bartoš <bartos.developer152@gmail.com>
2019-10-27 15:31:08 +0100

    [Http] Tests: container compilation

--- tests/generate-loader.php
+++ tests/generate-loader.php
@@ -1,7 +1,9 @@
 <?php
 
+use Tests\Modette\Http\HttpTestsHelper;
 use Tests\Modette\Monorepo\MonorepoTestsHelper;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 MonorepoTestsHelper::generateLoader();
+HttpTestsHelper::generateLoader();
